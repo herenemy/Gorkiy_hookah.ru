b9142a3b48b40509b100499876f4bdf64aa205b4
TIMUR CHELIK <slandarka22@gmail.com>
2024-03-13 21:35:52 +0300

    smooth section appearance, sticky header, added scroll functional to intro button and scroll to section on nav btn press

---
+++ src/js/script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const headerNode = document.querySelector('.header');
+const navNode = document.querySelector('.nav');
+const sectionReservationNode = document.querySelector('.section_reserv');
+const sectionIntroNode = document.querySelector('.section_intro');
+const btnScrollToNode = document.getElementById('btnScrollTo');
+const navLinksContainerNode = document.querySelector('.nav__items');
+const allSectionsNode = document.querySelectorAll('.appearance-section');
+
+btnScrollToNode.addEventListener('click', () => {
+  sectionReservationNode.scrollIntoView({ behavior: 'smooth' });
+});
+
+navLinksContainerNode.addEventListener('click', e => {
+  e.preventDefault();
+  const target = e.target;
+
+  if (target.classList.contains('nav__link')) {
+    const href = target.getAttribute('href');
+    document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
+    console.log('hello');
+  }
+});
+
+// Appearance section
+
+function appearanceSection(entries, observer) {
+  const entry = entries[0];
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section_hidden');
+  observer.unobserve(entry.target);
+}
+
+const sectionObserver = new IntersectionObserver(appearanceSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+allSectionsNode.forEach(item => {
+  sectionObserver.observe(item);
+  item.classList.add('section_hidden');
+});
+
+// Header observer
+const headerHeight = headerNode.getBoundingClientRect().height;
+const getStickyNav = entries => {
+  const entry = entries[0];
+  console.log(entry);
+  !entry.isIntersecting
+    ? headerNode.classList.add('nav_sticky')
+    : headerNode.classList.remove('nav_sticky');
+};
+
+const headerObserver = new IntersectionObserver(getStickyNav, {
+  root: null,
+  threshold: 0.1,
+  rootMargin: `-${headerHeight}px`,
+});
+headerObserver.observe(sectionIntroNode);
